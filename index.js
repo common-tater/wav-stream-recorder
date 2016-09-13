4bf9f915e35b9cf8aca616ff24be17e1882b56d7
Danny Bauman <dannybauman@gmail.com>
2016-09-13 12:12:52 -0700

    Fixed sizes and offsets for wav and riff headers, wav file plays properly!

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ module.exports = WavStreamRecorder
 
 var fs = require('fs')
 
+var FILE_CLOSE_TIMEOUT = 5000 // 5 secs of no data before we close the file and rewrite headers
+var HEADER_OFFSET_RIFF_SIZE = 4 // bytes 4-7 are the size of the riff header
+var HEADER_OFFSET_WAV_SIZE = 48
+var WAV_HEADER_SIZE = 52 // 52 bytes
+var RIFF_HEADER_SIZE = 8 // 8 bytes
+
 function WavStreamRecorder () {
   this.files = {}
   this.timers = {}
@@ -15,8 +21,8 @@ WavStreamRecorder.prototype.appendRecording = function (id, data, filepath) {
     clearTimeout(this.timers[id])
   }
 
-  // set the timer to close the file if we get no more data with 5 seconds
-  this.timers[id] = setTimeout(closeRecording.bind(this, id), 5000)
+  // set the timer to close the file if we get no more data within the timeout period
+  this.timers[id] = setTimeout(closeRecording.bind(this, id), FILE_CLOSE_TIMEOUT)
 
   // if the file's write stream has already been created, write to it
   if (this.files[id]) {
@@ -41,20 +47,24 @@ WavStreamRecorder.prototype.appendRecording = function (id, data, filepath) {
     if (file) {
       file.end()
 
+      // the riff and wav sizes will need to be converted to
+      // 4 byte buffers to be written to the file streams
       var riffSizeBuffer = new Buffer(4)
-      riffSizeBuffer.writeUInt32LE(file._length + 52, 0)
+      riffSizeBuffer.writeUInt32LE(file._length + WAV_HEADER_SIZE, 0)
       var wavSizeBuffer = new Buffer(4)
       wavSizeBuffer.writeUInt32LE(file._length, 0)
 
-      var fsWriteStream = fs.createWriteStream(filepath, {start: 4, flags: 'r+'})
-      fsWriteStream.on('open', function () {
-        fsWriteStream.write(riffSizeBuffer)
-        fsWriteStream.end()
+      // write the riff size header first
+      var riffSizeWriteStream = fs.createWriteStream(filepath, {start: HEADER_OFFSET_RIFF_SIZE, flags: 'r+'})
+      riffSizeWriteStream.on('open', function () {
+        riffSizeWriteStream.write(riffSizeBuffer)
+        riffSizeWriteStream.end()
 
-        var fsWriteStream2 = fs.createWriteStream(filepath, {start: 48, flags: 'r+'})
-        fsWriteStream2.on('open', function () {
-          fsWriteStream2.write(wavSizeBuffer)
-          fsWriteStream2.end()
+        // then write the wav size header
+        var wavSizeWriteStream = fs.createWriteStream(filepath, {start: HEADER_OFFSET_WAV_SIZE + RIFF_HEADER_SIZE, flags: 'r+'})
+        wavSizeWriteStream.on('open', function () {
+          wavSizeWriteStream.write(wavSizeBuffer)
+          wavSizeWriteStream.end()
         })
       })
 
@@ -66,11 +76,11 @@ WavStreamRecorder.prototype.appendRecording = function (id, data, filepath) {
     // if this isn't the first time we've written to this file,
     // strip the WAV/RIFF header from this chunk
     if (openFile._header) {
-      audioData = audioData.slice(60)
+      audioData = audioData.slice(WAV_HEADER_SIZE + RIFF_HEADER_SIZE)
       openFile._length += audioData.length
     } else {
       openFile._header = true
-      openFile._length = audioData.length - 60
+      openFile._length = audioData.length - (WAV_HEADER_SIZE + RIFF_HEADER_SIZE)
     }
 
     openFile.write(audioData)
